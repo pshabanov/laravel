284b226b8ef73b637cf3d57ae647e65e83a67835
pshabanov <pshabanov@nurtelecom.kg>
2023-04-19 15:10:12 +0600

    templates, posts, routing

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(){
+        $posts = Post::query()->orderBy('created_at', "DESC")->paginate(3);
+
+        return view('posts.index', [
+            'posts'=> $posts
+        ]);
+    }
+
+    public function detail($id){
+        $post = Post::query()->findOrFail($id);
+
+        return view('posts.detail', [
+            'post'=> $post
+        ]);
+    }
+}
